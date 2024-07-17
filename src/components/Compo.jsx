2f57f501c68f7cbfeef663d3cf99613d96eec127
lochinbek <lochinbekk77@gmail.com>
2024-07-17 14:10:33 +0500

    key

--- src/components/Compo.jsx
+++ src/components/Compo.jsx
@@ -5,14 +5,8 @@ function Compo() {
     <div className="div-1">
     {Compon.map((data)=>{
         return (
-            <div className="div-2  drop-shadow-md hover:drop-shadow-2xl">
-            {/* <img src={data.image} alt="" />
-            <h1>{data.lastName}</h1>
-            <h1>{data.region}</h1>
-            <h1>{data.email}</h1>
-            <h1>{data.firstName}</h1>
-            <h1>{data.phone}</h1>
-            <h1>{data.zip}</h1> */}
+            <div key={data.id} className="div-2  drop-shadow-md hover:drop-shadow-2xl">
+      
 <div class="card bg-base-100 shadow-xl">
   <figure>
     <img className="w-full"
